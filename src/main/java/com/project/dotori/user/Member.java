126faca34f358ab4ee8ba199c1135ed1f5ca9f0f
heenahan <mby1997s@gmail.com>
2024-03-09 00:43:36 +0900

    feat : member와 member book 도메인 설계

--- src/main/java/com/project/dotori/user/Member.java
+++ src/main/java/com/project/dotori/user/Member.java
@@ -1,10 +1,13 @@
 package com.project.dotori.user;
 
+import com.project.dotori.global.exception.BusinessException;
+import com.project.dotori.global.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,13 +19,22 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nickname", length = 50, nullable = false)
+    @Column(name = "nickname", length = 10, nullable = false)
     private String nickname;
 
     @Builder
     private Member(
         String nickname
     ) {
+        validNicknameLength(nickname);
         this.nickname = nickname;
+    }
+
+    private void validNicknameLength(
+        String nickname
+    ) {
+        if (!StringUtils.hasText(nickname) && nickname.length() > 10) {
+            throw new BusinessException(ErrorCode.INVALID_NICKNAME);
+        }
     }
 }
--- src/main/java/com/project/dotori/user/MemberBook.java
+++ src/main/java/com/project/dotori/user/MemberBook.java
@@ -1,10 +1,17 @@
 package com.project.dotori.user;
 
+import com.project.dotori.global.exception.BusinessException;
+import com.project.dotori.global.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,28 +29,70 @@ public class MemberBook {
     @Column(name = "book_id", nullable = false)
     private Long bookId;
 
-    @Column(name = "score", nullable = false)
-    private Integer score;
-
-    @Column(name = "review", length = 500, nullable = false)
-    private String review;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "status", length = 30, nullable = false)
     private MemberBookStatus memberBookStatus;
+
+    @Column(name = "score", nullable = true)
+    private Integer score;
+
+    @Column(name = "review", length = 500, nullable = true)
+    private String review;
 
     @Builder
     private MemberBook(
         Long memberId,
         Long bookId,
-        Integer score,
-        String review,
         MemberBookStatus memberBookStatus
     ) {
+        validIsNotNull(memberId, bookId, memberBookStatus);
         this.memberId = memberId;
         this.bookId = bookId;
+        this.memberBookStatus = memberBookStatus;
+    }
+
+    public void update(
+        Integer score,
+        String review
+    ) {
+        validScore(score);
+        validReview(review);
+
         this.score = score;
         this.review = review;
-        this.memberBookStatus = memberBookStatus;
+    }
+
+    private void validIsNotNull(
+        Long memberId,
+        Long bookId,
+        MemberBookStatus memberBookStatus
+    ) {
+        List<String> filedNames = new ArrayList<>();
+        if (Objects.isNull(memberId)) {
+            filedNames.add("memberId");
+        }
+        if (Objects.isNull(bookId)) {
+            filedNames.add("bookId");
+        }
+        if (Objects.isNull(memberBookStatus)) {
+            filedNames.add("memberBookStatus");
+            throw new BusinessException(ErrorCode.NOT_NULL, filedNames);
+        }
+    }
+
+    private void validScore(
+        Integer score
+    ) {
+        if (Objects.isNull(score) || score > 5 || score < 0) {
+            throw new BusinessException(ErrorCode.INVALID_SCORE);
+        }
+    }
+
+    private void validReview(
+        String review
+    ) {
+        if (!StringUtils.hasText(review) || review.length() > 500) {
+            throw new BusinessException(ErrorCode.INVALID_REVIEW);
+        }
     }
 }
