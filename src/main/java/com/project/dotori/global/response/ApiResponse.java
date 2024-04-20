3d4cd1e03bc4c7c264aacd117b0e8c07e5df09b6
heenahan <mby1997s@gmail.com>
2024-04-20 22:22:18 +0900

    test & refactor : api spec 변경 및 도서 API RestDocs 작성

--- src/main/java/com/project/dotori/global/response/ApiResponse.java
+++ src/main/java/com/project/dotori/global/response/ApiResponse.java
@@ -1,20 +1,28 @@
 package com.project.dotori.global.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 
     private final int statusCode;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private final LocalDateTime serverTime;
+
     private final T data;
 
     public static <T> ApiResponse<T> ok(T data) {
         return new ApiResponse<>(
             HttpStatus.OK.value(),
+            LocalDateTime.now(),
             data
         );
     }
@@ -22,6 +30,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> created(T data) {
         return new ApiResponse<>(
             HttpStatus.CREATED.value(),
+            LocalDateTime.now(),
             data
         );
     }
@@ -29,6 +38,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> noContent() {
         return new ApiResponse<>(
             HttpStatus.NO_CONTENT.value(),
+            LocalDateTime.now(),
             null
         );
     }
