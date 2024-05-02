f106889b8a6363cc4393094d37aacd9895db84d3
heenahan <mby1997s@gmail.com>
2024-05-02 23:14:50 +0900

    feat : 멤버 생성 로직 구현

---
+++ src/main/java/com/project/dotori/member/application/MemberService.java
@@ -0,0 +1,25 @@
+package com.project.dotori.member.application;
+
+import com.project.dotori.member.application.request.MemberCreateRequest;
+import com.project.dotori.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long createMember(
+        MemberCreateRequest request
+    ) {
+        var member = request.toEntity();
+        memberRepository.findBySocialIdAndRole(member.getSocialId(), member.getRole())
+            .orElseGet(() -> memberRepository.save(member));
+
+        return member.getId();
+    }
+}
