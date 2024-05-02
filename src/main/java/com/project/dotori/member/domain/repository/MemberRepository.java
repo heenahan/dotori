bdd34656be97f32acd608715bb890f31973f9c6e
heenahan <mby1997s@gmail.com>
2024-05-02 23:14:50 +0900

    feat : 멤버 생성 로직 구현

--- src/main/java/com/project/dotori/member/domain/repository/MemberRepository.java
+++ src/main/java/com/project/dotori/member/domain/repository/MemberRepository.java
@@ -1,7 +1,15 @@
 package com.project.dotori.member.domain.repository;
 
 import com.project.dotori.member.domain.Member;
+import com.project.dotori.member.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findBySocialIdAndRole(
+        String socialId,
+        Role roe
+    );
 }
