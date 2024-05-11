cf1a7f67d44260655155927c67f9ae75a48c9e7d
heenahan <mby1997s@gmail.com>
2024-05-11 22:20:22 +0900

    feat : cors 재설정

--- src/main/java/com/project/dotori/global/config/WebMvcConfig.java
+++ src/main/java/com/project/dotori/global/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import com.project.dotori.authorization.presentation.handler.MemberIdArgumentRes
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -28,7 +29,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-            .allowedOrigins("http://localhost:8080");
+            .allowedOrigins("http://localhost:8081")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.PUT.name()
+            )
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 
     @Override
